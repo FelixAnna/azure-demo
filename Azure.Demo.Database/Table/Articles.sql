64c840ad0bc4fa4bf5234f4f1dadf56ebe2c451a
Felix, Yu <felix.yu@schroders.com>
2018-11-27 10:23:24 +0800

    save changes

---
+++ Azure.Demo.Database/Table/Articles.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Articles]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(128) NOT NULL, 
+    [AuthorId] INT NULL, 
+    [Link] VARCHAR(1024) NOT NULL, 
+    [CreatedDate] DATETIME NOT NULL DEFAULT getutcdate()
+)
+GO
+
+ALTER TABLE [dbo].[Articles]
+	ADD CONSTRAINT [Article_Auther]
+	FOREIGN KEY ([AuthorId])
+	REFERENCES [dbo].[Authors](Id)
---
+++ Azure.Demo.Database/Table/Authors.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Authors]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Phone] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(128) NOT NULL, 
+    [CreatedDate] NCHAR(10) NOT NULL DEFAULT getutcdate()
+)
